67a60beb4415da3b96e55487a3ed1f8fc9640eb1
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-25 23:02:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,13 +4,14 @@ class App < Sinatra::Base
 
   get '/name' do
     @name = Name.all
+end
 
    get '/hometown' do
    @hometown = Hometown.all
+end
 
    get '/favorite-song' do
    @favorite-song = favorite-song.all
 end
-end
-end
+
 end
