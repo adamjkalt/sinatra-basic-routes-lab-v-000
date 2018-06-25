d8a83e8b5b1761339e167dcb8b2c68117ec7c7e9
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-25 23:01:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    @name = Name.all
+   
+   get '/hometown' do
+   @medicines = Medicine.all
+   get '/medicines' do
+   @medicines = Medicine.all
+   get '/medicines' do
+   @medicines = Medicine.all
+   get '/medicines' do
+   @medicines = Medicine.all
+  
+  
 end
